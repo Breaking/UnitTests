d6085d0867f1c8d5981e3e1931b0542fc8027c30
Breaking <makler_cs@tut.by>
2019-10-16 23:14:29 +0300

    delete unusued comments

--- app/src/main/java/com/krivosheev/mikhail/unittests/RepositoryImpl.kt
+++ app/src/main/java/com/krivosheev/mikhail/unittests/RepositoryImpl.kt
@@ -6,22 +6,14 @@ class RepositoryImpl(
     private val randomNameGenerator: RandomNameGenerator,
     private val cache: SparseArray<Backend.Result>
 ) : Repository {
-    // var cache = SparseArray<Backend.Result>()
 
-    override fun getResultFromCacheOrCreate(id: Int): Backend.Result {
-        //cache[id].name
-        //randomNameGenerator.generateRandomName()
-        return cache[id].takeIf { it != null } ?: {
+    override fun getResultFromCacheOrCreate(id: Int) =
+        cache[id].takeIf { it != null } ?: {
             val name = randomNameGenerator.generateRandomName()
             Backend.Result(name)
         }.invoke()
             .also {
                 cache.put(id, it)
             }
-    }
-
 }
 
-
-// 2 теста
-//1 тест в кеше пусто
